5f8f8b3f35d64c7468dcd3273408fc7cfa32bc7e
winnie48562000 <winnie48562000@gmail.com>
2021-07-05 14:03:43 +0800

    add README.md

---
+++ favorite.js
@@ -0,0 +1,76 @@
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
+
+const movies = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+
+const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input')
+
+// 電影清單
+function renderMovieList (data) {
+  let rawHTML = ''
+
+  data.forEach((item) => {
+    // console.log(item)
+    rawHTML += `
+      <div class="col-sm-3">
+        <div class="mb-2">
+          <div class="card">
+            <img class="card-img-top"
+              src="${POSTER_URL + item.image}"
+              alt="Movie Poster">
+            <div class="card-body">
+              <h5 class="movie-title">${item.title}</h5>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary btn-show-movie" data-toggle="modal"
+                data-target="#movie-modal" data-id="${item.id}">More</button>
+              <button class="btn btn-danger btn-remove-favorite" data-id="${item.id}">x</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+  })
+  dataPanel.innerHTML = rawHTML
+}
+
+// 彈跳式視窗(Modal)顯示電影資訊
+function showMovieModal(id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+
+  axios.get(INDEX_URL + id)
+    .then(response => {
+      const data = response.data.results
+      modalTitle.innerText = data.title
+      modalImage.innerHTML =`
+      <img src="${POSTER_URL + data.image}" alt="movie-poster" class="image-fluid">
+      `
+      modalDate.innerText = `Release date: ${data.release_date}`
+      modalDescription.innerText = data.description
+    })
+}
+
+function removeFromFavorite(id) {
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+  movies.splice(movieIndex, 1)
+  localStorage.setItem('favoriteMovies', JSON.stringify(movies))
+  renderMovieList(movies)
+}
+
+// 在button上綁定監聽器
+dataPanel.addEventListener('click', function onPanelClick(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    // console.log(event.target.dataset)
+    showMovieModal(Number(event.target.dataset.id))
+  } else if (event.target.matches('.btn-remove-favorite')) {
+      removeFromFavorite(Number(event.target.dataset.id))
+    }
+})
+
+renderMovieList(movies)
